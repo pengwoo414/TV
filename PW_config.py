1ae0c630323e31f2721025e0781e832a9825dd18
pengwoo414 <125797277+pengwoo414@users.noreply.github.com>
2024-04-20 19:55:09 +0800

    Create PW_config.py

---
+++ PW_config.py
@@ -0,0 +1,18 @@
+source_file = "PW_demo.txt"
+final_file = "PW_result.txt"
+favorite_list = [
+    "CCTV-1",
+    "CCTV-5",
+    "CCTV-5+",
+    "CCTV-13",
+    "五星体育",
+   ]
+favorite_page_num = 3
+default_page_num = 2
+urls_limit = 10
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 15
+ipv_type = "ipv6"
+domain_blacklist = ["epg.pw"]
+url_keywords_blacklist = []
